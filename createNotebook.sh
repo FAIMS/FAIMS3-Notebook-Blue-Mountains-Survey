5429d36b3f9a144505915003a0382636fc2d99b3
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2022-11-16 19:53:50 +1100

    removing required relationships
    Signed-off-by: Brian Ballsun-Stanton <denubis@noreply.users.github.com>

--- createNotebook.sh
+++ createNotebook.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-export SERVER_FULLNAME="${1:-dev.db.faims.edu.au}" # dev, alpha, testing, selenium
+export SERVER_FULLNAME="${1:-demo.db.faims.edu.au}" # dev, alpha, testing, selenium
 cp blue-mountains-survey.json faims3-temp-notebook-migrator/
 cd faims3-temp-notebook-migrator
 echo "Load Geochem ${SERVER_FULLNAME}"
 #sed -e "s/##SPID##/${SPID}/g" 20220110-model1.json > fiptemp.json
-bash loadNotebook.sh $SERVER_FULLNAME "Blue Mountains Survey $(date)" "blue-mountains-survey.json" "blue_mountains_survey"
+bash loadNotebook.sh $SERVER_FULLNAME "Jamison Valley Historic Mining Field Notebook" "blue-mountains-survey.json" "blue_mountains_survey"
 rm -f faims3-temp-notebook-migrator/mountains.json
 rm -f faims3-temp-notebook-migrator/testnotebook.json
